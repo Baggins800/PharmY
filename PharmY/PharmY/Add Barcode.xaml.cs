d0cee402e5f420703de303176df88aa035bb3dc3
Baggins <omega@live.co.za>
2018-02-24 21:12:21 +0200

    Add databse connection

--- PharmY/PharmY/Add Barcode.xaml.cs
+++ PharmY/PharmY/Add Barcode.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Data;
+using System.Configuration;
+
 
 namespace PharmY
 {
@@ -29,6 +33,31 @@ namespace PharmY
             var newForm = new Window1(); //create your new form admin.
             newForm.Show(); //show the new form.
             this.Close(); //only if you want to close the current form
+        }
+
+        private void btnadd_Click(object sender, RoutedEventArgs e)
+        {
+            
+            int barcode = 0;
+            if (!Int32.TryParse(edtaddbarcode.Text, out barcode))
+            {
+                MessageBox.Show("Please make sure the barcode is a number.");
+            }
+            else
+                using (OleDbConnection conn = new OleDbConnection(ConfigurationManager.ConnectionStrings["PharmY"].ConnectionString))
+                {
+                    //conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Baggins\\Desktop\\PharmY.mdb; Persist Security Info = False; ";
+                    OleDbCommand cmd = new OleDbCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "insert into ITEMS ([BARCODE_ID],[NAME]) values (?,?);";
+                    cmd.Parameters.AddWithValue("@BARCODE_ID", barcode);
+                    cmd.Parameters.AddWithValue("@NAME", edtaddbarcode.Text);
+                    cmd.Connection = conn;
+                    conn.Open();
+                    try { cmd.ExecuteNonQuery(); }
+                    catch (Exception enq) { MessageBox.Show(enq.Message); }
+
+                }
         }
     }
 }
