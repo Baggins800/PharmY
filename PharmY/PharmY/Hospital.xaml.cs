c357008a1452ddc74ee117931f2bf45a2ccc8183
Baggins <omega@live.co.za>
2018-02-27 20:00:27 +0200

    Update hospital

--- PharmY/PharmY/Hospital.xaml.cs
+++ PharmY/PharmY/Hospital.xaml.cs
@@ -11,13 +11,14 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace PharmY
 {
     /// <summary>
     /// Interaction logic for Window2.xaml
     /// </summary>
-    public partial class Hospital : Window
+    public partial class Hospital 
     {
         public Hospital()
         {
