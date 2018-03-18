ae5aeecf82545eaf01307ffcb93cd5a59f355d4c
anneke800 <lourensanneke1@gmail.com>
2018-03-18 23:24:29 +0200

    Update active ingredient in database

--- PharmY/PharmY/Add Active Ingridient.xaml.cs
+++ PharmY/PharmY/Add Active Ingridient.xaml.cs
@@ -37,28 +37,59 @@ namespace PharmY
 
         private void btnaddactive_Click(object sender, RoutedEventArgs e)
         {
-            string barcode = edtbarcode.Text;
-            if (barcode.Length < 1)
+            if (edtbarcode.Text.Length < 1 && edtactivename.Text.Length < 1)
             {
-                MessageBox.Show("Please make sure the barcode is a number.");
+                MessageBox.Show("Please make sure the barcode and ingredient ID is included.");
             }
             else
+            {
+                int activeExists = 0;
                 using (OleDbConnection conn = new OleDbConnection(ConfigurationManager.ConnectionStrings["PharmY"].ConnectionString))
                 {
-                    OleDbCommand add_barcode = new OleDbCommand();
-                    add_barcode.CommandType = CommandType.Text;
-                    add_barcode.CommandText = "insert into ACTIVE_INGREDIENTS ([INGREDIENT_ID],[BARCODE_ID]) values (?,?);";
-                    add_barcode.Parameters.AddWithValue("@INGREDIENT_ID", edtactivename.Text.ToUpper());
-                    add_barcode.Parameters.AddWithValue("@BARCODE_ID", barcode);
-                    add_barcode.Connection = conn;
+                    OleDbCommand count_active = new OleDbCommand();
+                    count_active.CommandType = CommandType.Text;
+                    count_active.CommandText = "select count (*) from ACTIVE_INGREDIENTS where [INGREDIENT_ID] = ?";
+                    count_active.Parameters.AddWithValue("@INGREDIENT_ID", edtactiveid.Text.ToUpper());
+                    count_active.Connection = conn;
                     conn.Open();
-                    try {
-                        add_barcode.ExecuteNonQuery();
+                    try { activeExists = (int)count_active.ExecuteScalar(); }
+                    catch (Exception enq) { MessageBox.Show(enq.Message); }
+                }
+                using (OleDbConnection conn = new OleDbConnection(ConfigurationManager.ConnectionStrings["PharmY"].ConnectionString))
+                {
+                    if (activeExists < 1)
+                    {
+                        OleDbCommand add_active = new OleDbCommand();
+                        add_active.CommandType = CommandType.Text;
+                        add_active.CommandText = "insert into ACTIVE_INGREDIENTS ([INGREDIENT_ID], [INGREDIENT_NAME]) values (?,?);";
+                        add_active.Parameters.AddWithValue("@INGREDIENT_ID", edtactiveid.Text.ToUpper());
+                        add_active.Parameters.AddWithValue("@INGREDIENT_NAME", edtactivename.Text.ToUpper());
+                        add_active.Connection = conn;
+                        conn.Open();
+                        try { add_active.ExecuteNonQuery(); }
+                        catch (Exception enq) { MessageBox.Show(enq.Message); }
+                    }
+                }
+                using (OleDbConnection conn = new OleDbConnection(ConfigurationManager.ConnectionStrings["PharmY"].ConnectionString))
+                {
+                    OleDbCommand add_active = new OleDbCommand();
+                    add_active.CommandType = CommandType.Text;
+                    add_active.CommandText = "insert into ITEM_INGREDIENTS ([BARCODE_ID],[INGREDIENT_ID]) values (?,?);";
+                    add_active.Parameters.AddWithValue("@BARCODE_ID", edtbarcode.Text.ToUpper());
+                    add_active.Parameters.AddWithValue("@INGREDIENT_ID", edtactiveid.Text.ToUpper());
+                    add_active.Connection = conn;
+                    conn.Open();
+                    try
+                    {
+                        add_active.ExecuteNonQuery();
                         edtbarcode.Text = "";
+                        edtactiveid.Text = "";
                         edtactivename.Text = "";
                     }
                     catch (Exception enq) { MessageBox.Show(enq.Message); }
+
                 }
+            }
         }
     }
 }
